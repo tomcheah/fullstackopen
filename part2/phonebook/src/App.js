32c61419266869647d732b83af499198dc8a99e0
Tom Cheah <tomcheah@berkeley.edu>
2022-06-08 23:35:35 -0700

    Finish exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import { v4 as uuid } from 'uuid';
 
 
-const PersonsDisplay = ( {name} ) => <div>{name}</div>
+const PersonsDisplay = ( {name, number} ) => <div>{name} {number}</div>
 
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: uuid() }
+    { name: 'Arto Hellas', id: uuid(), number: "123-567-7890" }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => { 
     event.preventDefault()
@@ -22,17 +23,25 @@ const App = () => {
     const nameObject = {
       name: newName,
       id: uuid(),
+      number: newNumber,
     }
     console.log(nameObject)
 
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+  
   
 // todo: fix how persons are displayed/ rendered
   return (
@@ -43,11 +52,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <PersonsDisplay key={person.id} name={person.name} />)}
+      {persons.map(person => <PersonsDisplay key={person.id} name={person.name} number={person.number}/>)}
     </div>
   )
 }
