12fd76d2ada55251b0d589b1aff1a1c8a035e00d
Tom Cheah <tomcheah@berkeley.edu>
2022-06-20 20:05:43 -0700

    Finish exercise 3.4

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -31,7 +31,11 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    if (persons) {
+        response.json(persons)
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -49,6 +53,13 @@ app.get('/info', (request, response) => {
     response.write(`<h1>Phonebook has info for ${persons.length} people</h1>`)
     response.write(`<p>${new Date()}</p>`)
     response.send()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(note => note.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
