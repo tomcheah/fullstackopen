d88b7d5c767d3d46b5802edc514c5c6a2c8122aa
Tom Cheah <tomcheah@berkeley.edu>
2022-06-24 19:15:36 -0700

    Finish exercise 4.1

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -7,8 +7,8 @@ const cors = require('cors')
 const Person = require('./models/person')
 
 app.use(express.json())
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :response-time ms :body'));
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :response-time ms :body'))
 app.use(cors())
 app.use(express.static('build'))
 
@@ -30,16 +30,17 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    Person.countDocuments({}, function (err, count) {
-      response.write(`<h1>Phonebook has info for ${count} people</h1>`)
-      response.write(`<p>${new Date()}</p>`)
-      response.send()
-    });
+  Person.countDocuments({}, function (err, count) {
+    response.write(`<h1>Phonebook has info for ${count} people</h1>`)
+    response.write(`<p>${new Date()}</p>`)
+    response.send()
+  })
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then(result => {
+    .then((result) => {
+      console.log(result)
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -48,7 +49,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.put('/api/persons/:id', (request, response, next) => {
   const { name, number } = request.body
 
-  Person.findByIdAndUpdate(request.params.id, { name, number }, {new: true, runValidators: true, context: 'query'})
+  Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       console.log(updatedPerson)
       response.json(updatedPerson)
@@ -57,18 +58,24 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    
-    if (!body.name || !body.number) {
-        return response.status(400).json({ error: 'name or number missing' })
+  const body = request.body
+
+  // Name exists in phonebook already
+  Person.findOne({ name: body.name }, function(error, person) {
+    if (error) {
+      return response.status(440).send({ error: error.message })
     }
+    if (person) {
+      return response.status(400).send({ error: 'name already exists in phonebook, use update functionality instead' })
+    }
+  })
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    person.save()
+  person.save()
     .then(savedPerson => {
       response.json(savedPerson)
     })
@@ -76,13 +83,13 @@ app.post('/api/persons', (request, response, next) => {
 })
 
 const errorHandler = (error, request, response, next) => {
-  if (error.name === "CastError") {
-    return response.status(400).send({error: 'malformatted id'})
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
 
-  next(error) 
+  next(error)
 }
 
 // errorHandler MUST be last loaded middleware and has to below all the APIs
