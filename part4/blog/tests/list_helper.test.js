b0534e720693a7f383999da2d2b69d03701b1cdc
Tom Cheah <tomcheah@berkeley.edu>
2022-06-26 21:18:09 -0700

    Finish exercises 4.6 - 4.7

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -117,3 +117,121 @@ describe('total likes', () => {
         expect(result).toEqual(listWithMultipleBlogs[1])
     })
 })
+
+describe('most blogs', () => {
+  const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Bedbean Explores Rito',
+        author: 'Bedbean',
+        url: 'test.url',
+        likes: 49824,
+        __v: 0
+      }
+    ]
+  
+    const listWithMultipleBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Bedbean Explores Rito',
+          author: 'Bedbean',
+          url: 'test.url',
+          likes: 2,
+          __v: 0
+        },
+        {
+            _id: '2498723489789274',
+            title: 'Jett Takes on Pearl',
+            author: 'Jett',
+            url: 'test.url',
+            likes: 300,
+            __v: 0
+          },
+          {
+            _id: '1029834982734',
+            title: 'Eating Food',
+            author: 'Jett',
+            url: 'test.url',
+            likes: 1,
+            __v: 0
+          }
+      ]
+
+    const answer1 = {author: 'Bedbean', blogs: 1}
+    const answer2 = {author: 'Jett', blogs: 2}
+
+    test('empty list', () => {
+        const result = listHelper.mostBlogs([])
+        expect(result).toBe("Blogs do not exist")
+    })
+  
+    test('list with one blog', () => {
+      const result = listHelper.mostBlogs(listWithOneBlog)
+      expect(result).toEqual(answer1)
+  })
+
+  test('list with multiple blogs', () => {
+    const result = listHelper.mostBlogs(listWithMultipleBlogs)
+    expect(result).toEqual(answer2)
+})
+
+})
+
+describe('most likes', () => {
+  const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Bedbean Explores Rito',
+        author: 'Bedbean',
+        url: 'test.url',
+        likes: 49824,
+        __v: 0
+      }
+    ]
+  
+    const listWithMultipleBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Bedbean Explores Rito',
+          author: 'Bedbean',
+          url: 'test.url',
+          likes: 2,
+          __v: 0
+        },
+        {
+            _id: '2498723489789274',
+            title: 'Jett Takes on Pearl',
+            author: 'Jett',
+            url: 'test.url',
+            likes: 300,
+            __v: 0
+          },
+          {
+            _id: '1029834982734',
+            title: 'Eating Food',
+            author: 'Jett',
+            url: 'test.url',
+            likes: 1,
+            __v: 0
+          }
+      ]
+
+    const answer1 = {author: 'Bedbean', likes: 49824}
+    const answer2 = {author: 'Jett', likes: 301}
+
+    test('empty list', () => {
+        const result = listHelper.mostLikes([])
+        expect(result).toBe("Blogs do not exist")
+    })
+  
+    test('list with one blog', () => {
+      const result = listHelper.mostLikes(listWithOneBlog)
+      expect(result).toEqual(answer1)
+  })
+
+  test('list with multiple blogs', () => {
+    const result = listHelper.mostLikes(listWithMultipleBlogs)
+    expect(result).toEqual(answer2)
+})
+
+})
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -17,7 +19,59 @@ const favoriteBlog = (blogs) => {
     }
     return blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
 }
+
+const mostBlogs = (blogs) => {
+    if (blogs === undefined || blogs.length === 0) {
+        return "Blogs do not exist"
+    }
+
+    let blogCounts = blogs.reduce((acc, blog) => {
+        if (!acc[blog.author]) {
+            acc[blog.author] = 0
+        }
+        acc[blog.author] ++
+        return acc
+    }, {})
+
+    maxBlogCount = Math.max(...Object.values(blogCounts))
+
+    // Find the author with the same blog count
+    const asArray = Object.entries(blogCounts)
+    const filtered = asArray.filter(([author, blogCount]) => blogCount === maxBlogCount);
+    authorObject = Object.fromEntries(filtered)
+
+    authorObject = Object.entries(authorObject)[0]
+    return {'author': authorObject[0], 'blogs': authorObject[1]}
+}
+
+const mostLikes = (blogs) => {
+    if (blogs === undefined || blogs.length === 0) {
+        return "Blogs do not exist"
+    }
+
+    // First create a map of author: [like_count, like_count]
+    let grouped = _.reduce(blogs, (result, blog) => {
+        (result[blog.author] || (result[blog.author] = [])).push(blog.likes);
+        return result;
+    }, {});
+
+    // Then reduce author: [like_count]
+    let authorLikesCount = {}
+    for (const [author, likesArray] of Object.entries(grouped)) {
+        authorLikesCount[author] = likesArray.reduce((prev, curr) => prev + curr, 0)
+    }
+    // Then get max like count
+    maxLikeCount = Math.max(...Object.values(authorLikesCount))
+
+    // Find the author with the same like count
+    const asArray = Object.entries(authorLikesCount);
+    const filtered = asArray.filter(([author, likeCount]) => likeCount === maxLikeCount);
+    authorObject = Object.fromEntries(filtered)
+
+    authorObject = Object.entries(authorObject)[0]
+    return {'author': authorObject[0], 'likes': authorObject[1]}
+}
   
 module.exports = {
-    dummy, totalLikes, favoriteBlog
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
