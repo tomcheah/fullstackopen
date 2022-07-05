09fecbdd9603e89c26af08bc46797e7484f0f0fa
Tom Cheah <tomcheah@berkeley.edu>
2022-07-04 22:53:29 -0700

    Finish exercise 4.8

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('express-async-errors')
 
 const http = require('http')
 const express = require('express')
@@ -7,7 +8,10 @@ const blogsRouter = require('./controllers/blog')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
 
-const mongoUrl = config.MONGODB_URI
+const mongoUrl = process.env.NODE_ENV === 'test' 
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
+
 mongoose
     .connect(mongoUrl)
     .then(result => {
---
+++ part4/blog/tests/test_helper.js
@@ -0,0 +1,40 @@
+const Blog = require("../models/blog")
+
+const initialBlogs = [
+    {
+        title: 'Bedbean Explores Rito',
+        author: 'Bedbean',
+        url: 'test.url',
+        likes: 2,
+    },
+    {
+        title: 'Jett Takes on Pearl',
+        author: 'Jett',
+        url: 'test.url',
+        likes: 300,
+    },
+    {
+        title: 'Eating Food',
+        author: 'Bread',
+        url: 'test.url',
+        likes: 1,
+    }
+]
+
+const nonExistingId = async () => {
+    const blog = new Note({ title: 'willremovethissoon', author: 'test', url: "url", likes: 3 })
+    await blog.save()
+    await blog.remove()
+  
+    return blog._id.toString()
+  }
+
+const blogsInDb = async () => {
+    const blogs = await Blog.find({})
+    return blogs.map(blog => blog.toJSON())
+  }
+  
+
+module.exports = {
+    initialBlogs, nonExistingId, blogsInDb
+}
