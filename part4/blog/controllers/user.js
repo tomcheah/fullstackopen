f69553c1e9c0e39ddc8292a3071072542d0f06fb
Tom Cheah <tomcheah@berkeley.edu>
2022-07-10 20:48:37 -0700

    Finsih exercises 4.16 - 4.17

--- part4/blog/controllers/user.js
+++ part4/blog/controllers/user.js
@@ -3,12 +3,24 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1, id: 1})
     response.json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
     const {username, name, password } = request.body
+
+    if (username === undefined || password === undefined) {
+        return response.status(400).json({
+            error: 'username and password must be given'
+        })
+    }
+
+    if (username.length < 3 || password.length < 3) {
+        return response.status(400).json({
+            error: 'username and password must be each longer than 3 characters'
+        })
+    }
 
     const existingUser = await User.findOne({ username })
     if (existingUser) {
--- part4/blog/tests/user_api.test.js
+++ part4/blog/tests/user_api.test.js
@@ -40,8 +40,86 @@ describe('when there is initially one user in db', () => {
     expect(usernames).toContain(newUser.username)
   })
 
+  test('creation fails with a repeated username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'bedbean',
+      name: 'bed bean',
+      password: 'kirby',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+
+    const usersCheck = await helper.usersInDb()
+    expect(usersCheck).toHaveLength(usersAtStart.length + 1)
+  })
+
+  test('creation fails with invalid username or password', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const noPassword = {
+      username: 'noPassword',
+      name: 'no password',
+    }
+
+    const noUsername = {
+      name: 'noUsername',
+      password: 'userasdf',
+    }
+
+    const invalidUsername = {
+      username: 'b',
+      name: 'invalid username',
+      password: 'kirby',
+    }
+
+    const invalidPassword = {
+      username: 'invalidPassword',
+      name: 'invalid password',
+      password: 'a',
+    }
+
+    await api
+      .post('/api/users')
+      .send(noPassword)
+      .expect(400)
+
+    await api
+      .post('/api/users')
+      .send(noUsername)
+      .expect(400)
+
+    await api
+      .post('/api/users')
+      .send(invalidPassword)
+      .expect(400)
+
+    await api
+      .post('/api/users')
+      .send(invalidUsername)
+      .expect(400)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+
   afterAll(() => {
     mongoose.connection.close()
   })
 })
-
