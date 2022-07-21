92b79dab48d614346985489a9dd695e4f654f195
Tom Cheah <tomcheah@berkeley.edu>
2022-07-20 18:32:14 -0700

    Finish exercise 5.3

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -9,6 +9,9 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   const handleLogout = (event) => {
     event.preventDefault()
@@ -16,6 +19,22 @@ const App = () => {
     setUser(null)
     setUsername('')
     setPassword('')
+  }
+
+  const handleNewBlog = async (event) => {
+    event.preventDefault()
+    try {
+      console.log(title)
+      await blogService.create({title, author, url})
+      blogService.getAll().then(blogs =>
+        setBlogs( blogs )
+      )
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch (exception) {
+      console.log(exception)
+    }
   }
 
   const handleLogin = async (event) => {
@@ -82,8 +101,44 @@ const App = () => {
   </div>
   )
 
-  const blogsList = () => (
+  const newBlogForm = () => (
+    <div>
+      <h2>create new blog</h2>
+      <form onSubmit={handleNewBlog}>
       <div>
+        title
+          <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author
+          <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url
+          <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">submit</button>
+  </form>
+  </div>
+)
+
+  const blogsList = () => (
+    <div>
       <h2>blogs</h2>
       <div>{user.username} logged in
         <button onClick={handleLogout}>logout</button>
@@ -93,9 +148,11 @@ const App = () => {
       )}
     </div>
   )
+
   return (
     <div>
-      {user === null ? loginForm() : blogsList() }
+      {user === null ? loginForm() : blogsList()}
+      {user !== null && newBlogForm()}
     </div>
   )
 }
