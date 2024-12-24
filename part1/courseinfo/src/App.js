da570ecd1d1361f0087adeae7133cdf5026237f5
Tom Cheah <tomcheah316@gmail.com>
2024-12-24 15:27:36 -0800

    Revisit exercises 1.3 - 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -25,10 +25,15 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-  console.log(props.parts)
+  let total = 0
+  props.parts.forEach(part => {
+    total += part.exercises
+  });
+  console.log("This is prop.parts: ", props.parts)
+  console.log(`prop.parts has ${total} total exercises`)
   return (
     <div> 
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <p>Number of exercises {total}</p>
     </div>
   )
 }
